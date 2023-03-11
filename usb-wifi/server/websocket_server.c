e708f89fccbf9c080edca1df34d6f06fbdf65114
salgue441 <carlossalguero441@gmail.com>
2023-03-11 17:09:56 -0600

    Implemented the base code for processing data through usb

---
+++ usb-wifi/server/websocket_server.c
@@ -0,0 +1,25 @@
+/**
+ * @file usb_communication.c
+ * @author Carlos Salguero
+ * @brief Websocket server for the communication with the client
+ * @version 0.1
+ * @date 2023-03-11
+ *
+ * @copyright Copyright (c) 2023 - MIT License
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <libwebsockets.h>
+
+/**
+ * @brief
+ * Main function for the websocket server
+ * @param argc - number of arguments
+ * @param argv - arguments
+ * @return int - 0 if success
+ * @return int - 1 if error
+ */
+int main(int argc, char **argv)
+{
+}
