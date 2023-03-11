5d5f7dd632b07c18682f996892deb88e0a070be4
salgue441 <carlossalguero441@gmail.com>
2023-03-11 16:57:57 -0600

    Initial state of the cmake configuration for the project

---
+++ usb-wifi/build/server/CMakeFiles/websocket_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/websocket_server.dir/websocket_server.c.o"
+  "CMakeFiles/websocket_server.dir/websocket_server.c.o.d"
+  "websocket_server"
+  "websocket_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/websocket_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
